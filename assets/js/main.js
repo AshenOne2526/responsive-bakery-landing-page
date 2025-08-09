5460aaff5de0e3aa83d42e4dc3bb4abdba23a58b
AshenOne2526 <SheremetevEastSide@gmail.com>
2025-08-09 17:51:40 +0300

    Add Header and Nav section, new images and scrollreveal js

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toogle'),
+      navClose = document.getElementById('nav-close')
+
+/* Menu show */
+if(navToggle){
+   navToggle.addEventListener('click', () =>{
+      navMenu.classList.add('show-menu');
+   })
+}
+
+/* Menu hidden */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+      navMenu.classList.remove('show-menu');
+   })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link');
+
+/* When we click on each link, we remove the show-menu class*/
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/*=============== ADD BLUR HEADER ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
